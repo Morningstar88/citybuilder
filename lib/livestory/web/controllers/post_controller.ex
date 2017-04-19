b4dc2d0e42a4fc49266f135bd19d4fedcb0f8fe5
mikeflow <alphauserinterface@gmail.com>
2017-04-19 07:08:08 +0000

    changed buttons

--- lib/livestory/web/controllers/post_controller.ex
+++ lib/livestory/web/controllers/post_controller.ex
@@ -77,7 +77,7 @@ end
     case Stories.update_post(post, post_params) do
       {:ok, post} ->
         conn
-        |> put_flash(:info, "Post created/updated.")
+        |> put_flash(:info, "Post created/updated. Ψ ")
         |> redirect(to: post_path(conn, :show, post))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", post: post, changeset: changeset)

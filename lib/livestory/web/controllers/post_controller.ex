9ba8bf7f364db2c19bfa5d73295766e2aa83156f
denispeplin <denis.peplin@gmail.com>
2017-04-24 11:59:48 +0300

    User can change and remove only his own posts.

--- lib/livestory/web/controllers/post_controller.ex
+++ lib/livestory/web/controllers/post_controller.ex
@@ -5,11 +5,13 @@ defmodule LiveStory.Web.PostController do
   alias LiveStory.Web.ErrorHandler
 
   plug Guardian.Plug.EnsureAuthenticated, [handler: ErrorHandler] when not action in [:index]
+  plug :set_user
+  plug :set_post when action in [:fork, :show, :edit, :update, :delete]
+  plug :check_same_user when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
-    user = Guardian.Plug.current_resource(conn)
     posts = Stories.list_posts() #Original code
-    render(conn, "index.html", posts: posts, user: user)
+    render(conn, "index.html", posts: posts, user: conn.assigns.user)
   end
 
   #Udia Web Controllers
@@ -29,8 +31,7 @@ defmodule LiveStory.Web.PostController do
   end
 
   def create(conn, %{"post" => post_params}) do
-    user = Guardian.Plug.current_resource(conn)
-    case Stories.create_post(post_params, user) do
+    case Stories.create_post(post_params, conn.assigns.user) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post created! ヽ(´▽`)/")
@@ -40,22 +41,18 @@ defmodule LiveStory.Web.PostController do
     end
   end
 
- def fork(conn, %{"id" => id}) do
-    post = Stories.get_post!(id)
-    user = Guardian.Plug.current_resource(conn)
-    IO.inspect post
-    forked_post = Stories.create_forked_post(post, user)
+  def fork(conn, _params) do
+    forked_post = Stories.create_forked_post(conn.assigns.post, conn.assigns.user)
     changeset = Stories.change_post(forked_post)
-    changeset = Stories.change_post(post)
-    render(conn, "edit.html", post: post, changeset: changeset)
-end
- 
- 
- 
-  # Old Forked post code 
+    render(conn, "edit.html", post: forked_post, changeset: changeset)
+  end
+
+
+
+  # Old Forked post code
   # def fork(conn, %{"id" => id}) do
   #     post = Stories.get_post!(id)
-  #     forked_post = post 
+  #     forked_post = post
   #       {:ok, forked_post} ->
   #         conn
   #         |> put_flash(:info, :"You just forked that post.")
@@ -65,35 +62,49 @@ end
   #   end
   # # end
 
-  def show(conn, %{"id" => id}) do
-    post = Stories.get_post!(id)
-    render(conn, "show.html", post: post)
+  def show(conn, _params) do
+    render(conn, "show.html", post: conn.assigns.post)
   end
 
-  def edit(conn, %{"id" => id}) do
-    post = Stories.get_post!(id)
-    changeset = Stories.change_post(post)
-    render(conn, "edit.html", post: post, changeset: changeset)
+  def edit(conn, _params) do
+    changeset = Stories.change_post(conn.assigns.post)
+    render(conn, "edit.html", post: conn.assigns.post, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Stories.get_post!(id)
-
-    case Stories.update_post(post, post_params) do
+  def update(conn, %{"post" => post_params}) do
+    case Stories.update_post(conn.assigns.post, post_params) do
       {:ok, post} ->
         conn
      #  |> put_flash(:info, "Post created/updated. Ψ ")
         |> redirect(to: post_path(conn, :show, post))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", post: post, changeset: changeset)
+        render(conn, "edit.html", post: conn.assigns.post, changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    post = Stories.get_post!(id)
-    {:ok, _post} = Stories.delete_post(post)
+  def delete(conn, _params) do
+    {:ok, _post} = Stories.delete_post(conn.assigns.post)
     conn
     |> put_flash(:info, "Post deleted successfully.")
     |> redirect(to: post_path(conn, :index))
+  end
+
+  defp set_user(conn, _opts) do
+    assign(conn, :user, Guardian.Plug.current_resource(conn))
+  end
+
+  defp set_post(conn, _opts) do
+    %{"id" => id} = conn.params
+    assign(conn, :post, Stories.get_post!(id))
+  end
+
+  defp check_same_user(conn, _opts) do
+    if conn.assigns.user.id != conn.assigns.post.user_id do
+      conn
+      |> put_flash(:error, "This post belongs to other user.")
+      |> redirect(to: post_path(conn, :index))
+    else
+      conn
+    end
   end
 end

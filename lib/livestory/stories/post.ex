a4e29326989dff931e3eacfdacc71d4a62b213d5
denispeplin <denis.peplin@gmail.com>
2017-05-02 09:43:53 +0300

    Remove forks table, use original_post_id.

--- lib/livestory/stories/post.ex
+++ lib/livestory/stories/post.ex
@@ -6,8 +6,7 @@ defmodule LiveStory.Stories.Post do
     field :title, :string
     field :published, :boolean, default: true
     belongs_to :user, LiveStory.Auths.User
-    has_one :original_link, LiveStory.Stories.ForkedPost, foreign_key: :forked_post_id
-    has_one :original_post, through: [:original_link, :original_post]
+    belongs_to :original_post, LiveStory.Stories.Post
 
     timestamps()
   end

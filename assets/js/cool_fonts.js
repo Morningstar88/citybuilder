af9159efaf18e18f2c20f09a4b9b09206f19d3b7
mikeflow <contactwithagartha@gmail.com>
2017-05-24 22:24:11 +0000

    change font select menu to orange

--- assets/js/cool_fonts.js
+++ assets/js/cool_fonts.js
@@ -1,40 +1,37 @@
-// console.clear(); // Is this line causing font storage to wipe on page refresh?
-// var fontsArr = ['Raleway', 'Josefin Sans', 'Montserrat', 'Quicksand', 'Marck Script'];
-// var $fontSelector = $('.font-selector select');
-// var $preview = $('.preview-title');
-// var $fontTextSelector = $('.font-selector-text select');
-// var $previewText = $('.preview-text');
-// var $previewChoose = $('.preview-text-choose-font');
-// $fontSelector.on('change', function() {
-//   $(this).css({
-//     fontFamily: $(this).val()
-//   });
-//   $preview.css({
-//     fontFamily: $(this).val()
-//   });
-//   $previewChoose({
-//     fontFamily: $(this).val()
-//   });
-// });
-// $fontTextSelector.on('change', function() {
-//   $(this).css({
-//     fontFamily: $(this).val()
-//   });
-//   $previewText.css({
-//     fontFamily: $(this).val()
-//   });
-// });
-// _.forEach(fontsArr, function(fontName, index){
-//   var $option = $('<option style="font-family:'+fontName+'">'+fontName+'</option>');
-//   $fontSelector.append($option);
-// });
-// _.forEach(fontsArr, function(fontName, index){
-//   var $option = $('<option style="font-family:'+fontName+'">'+fontName+'</option>');
-//   $fontTextSelector.append($option);
-// });
-// $fontSelector.trigger('change');
-// WebFont.load({
-//   google: {
-//     families: fontsArr 
-//   }
-// });
+console.clear(); // Is this line causing font storage to wipe on page refresh?
+var fontsArr = ['Raleway', 'Josefin Sans', 'Montserrat', 'Quicksand', 'Marck Script'];
+var $fontSelector = $('.font-selector select');
+var $preview = $('.preview-title');
+var $fontTextSelector = $('.font-selector-text select');
+var $previewText = $('.preview-text');
+var $previewChoose = $('.preview-text-choose-font');
+$fontSelector.on('change', function() {
+  $(this).css({
+    fontFamily: $(this).val()
+  });
+  $preview.css({
+    fontFamily: $(this).val()
+  });
+});
+$fontTextSelector.on('change', function() {
+  $(this).css({
+    fontFamily: $(this).val()
+  });
+  $previewText.css({
+    fontFamily: $(this).val()
+  });
+});
+_.forEach(fontsArr, function(fontName, index){
+  var $option = $('<option style="font-family:'+fontName+'">'+fontName+'</option>');
+  $fontSelector.append($option);
+});
+_.forEach(fontsArr, function(fontName, index){
+  var $option = $('<option style="font-family:'+fontName+'">'+fontName+'</option>');
+  $fontTextSelector.append($option);
+});
+$fontSelector.trigger('change');
+WebFont.load({
+  google: {
+    families: fontsArr 
+  }
+});

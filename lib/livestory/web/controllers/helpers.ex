3f06fcc5f3409c3a6b78a74633ab948c53f4899a
denispeplin <denis.peplin@gmail.com>
2017-05-27 09:48:22 +0300

    Check if user can modify comment before showing links to him.

--- lib/livestory/web/controllers/helpers.ex
+++ lib/livestory/web/controllers/helpers.ex
@@ -4,4 +4,8 @@ defmodule LiveStory.Web.Controller.Helpers do
     |> DateTime.from_naive!("Etc/UTC")
     |> Timex.format!("{ANSIC}")
   end
+
+  def can_modify_comment?(user, comment) do
+    user.id == comment.user_id
+  end
 end

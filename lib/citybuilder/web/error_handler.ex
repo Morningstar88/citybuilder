71f2b23a9097f9314d4672e97326221f25a8ec82
mikeflow <contactwithagartha@gmail.com>
2017-08-13 00:09:15 +0000

    add colors to title and display boxes

--- lib/citybuilder/web/error_handler.ex
+++ lib/citybuilder/web/error_handler.ex
@@ -3,7 +3,7 @@ defmodule Citybuilder.Web.ErrorHandler do
 
   def unauthenticated(conn, _params) do
     conn
-    |> put_flash(:error, "You need to login to write! Please signup or log in :)")
+    |> put_flash(:error, "You need to login to start a project! Please signup or log in :)")
     |> redirect(to: user_path(conn, :signin))
   end
 end

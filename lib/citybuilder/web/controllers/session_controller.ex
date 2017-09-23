92c32821d3b394636bdc7afaf2c3b9dc81185176
mikeflow <contactwithagartha@gmail.com>
2017-09-23 12:41:43 +0000

    add flash after user logs on

--- lib/citybuilder/web/controllers/session_controller.ex
+++ lib/citybuilder/web/controllers/session_controller.ex
@@ -9,6 +9,7 @@ defmodule Citybuilder.Web.SessionController do
       {:ok, user} ->
         conn
         |> Guardian.Plug.sign_in(user)
+        |> put_flash(:info, "Welcome to Citybuilder! Plz go ahead and build something awesome") 
         |> redirect(to: "/")
 
       {:error, _err} ->
@@ -20,14 +21,14 @@ defmodule Citybuilder.Web.SessionController do
 
   def unauthenticated(conn, params) do
     conn
-    |> put_flash(:info, "Sign Up failed.")
+    |> put_flash(:info, "Sorry! Sign Up failed.")
     |> redirect(to: "/")
   end
 
   def logout(conn, _params) do
     conn
     |> Guardian.Plug.sign_out
-    |> put_flash(:info, "Logged out. cu later!")
+    |> put_flash(:info, "Logged out. CU later!")
     |> redirect(to: "/")
   end
 end
